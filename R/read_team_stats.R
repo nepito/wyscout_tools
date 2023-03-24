5a84d5c076407a8de3b203b13975bb478223dbbe
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-24 12:10:02 -0700

    ✍🏿 Add documentation

--- R/read_team_stats.R
+++ R/read_team_stats.R
@@ -1,5 +1,10 @@
+#' Read the team stats of wyscout data
+#'
+#' @param path A character with the path of the data.
+#'
+#' @return A tibble with the stats of some team.
+#' @export
 read_team_stats <- function(path) {
-  team <- readr::read_csv(path, show_col_types = FALSE)
-  names(team) <- all_names$column_names
+  team <- readr::read_csv(path, show_col_types = FALSE, col_names = all_names$column_names)
   return(team)
 }

537a70fa6c34e16eebb75708af3183d3550240f7
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-24 12:18:17 -0700

    ✅ Pass test

--- R/read_team_stats.R
+++ R/read_team_stats.R
@@ -5,6 +5,6 @@
 #' @return A tibble with the stats of some team.
 #' @export
 read_team_stats <- function(path) {
-  team <- readr::read_csv(path, show_col_types = FALSE, col_names = all_names$column_names)
+  team <- readr::read_csv(path, show_col_types = FALSE, col_names = all_names$column_names, skip = 1)
   return(team)
 }

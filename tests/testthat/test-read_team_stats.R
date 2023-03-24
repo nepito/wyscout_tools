8fb716b1f5454d15e75b504f2525df9cd5679147
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-23 23:32:18 -0700

    ✅🪲 Fix test: Fix file name and dependency

--- tests/testthat/test-read_team_stats.R
+++ tests/testthat/test-read_team_stats.R
@@ -1,5 +1,7 @@
+library(tidyverse)
+
 describe("read data with the right names", {
-  liverpool <- read_team_stats("/workdir/tests/data/liverpool.csv")
+  liverpool <- read_team_stats("/workdir/tests/data/Liverpool.csv")
   expected <- read_csv("/workdir/tests/data/team_stats_column_names.csv")
   obtained <- names(liverpool)
   expect_equal(obtained, expected$column_names)

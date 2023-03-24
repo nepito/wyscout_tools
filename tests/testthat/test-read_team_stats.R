1c179a559e70ae3696b82e732e4551955fece839
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-23 23:36:21 -0700

    🤫 Silent warning

--- tests/testthat/test-read_team_stats.R
+++ tests/testthat/test-read_team_stats.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 describe("read data with the right names", {
   liverpool <- read_team_stats("/workdir/tests/data/Liverpool.csv")
-  expected <- read_csv("/workdir/tests/data/team_stats_column_names.csv")
+  expected <- read_csv("/workdir/tests/data/team_stats_column_names.csv", show_col_types = FALSE)
   obtained <- names(liverpool)
   expect_equal(obtained, expected$column_names)
 })

58f8baebb7c7c26c8d4bab728b5cff1e8450db75
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-23 23:36:48 -0700

    ✅ Pass test

---
+++ R/read_team_stats.R
@@ -0,0 +1,6 @@
+read_team_stats <- function(path) {
+  team <- readr::read_csv(path, show_col_types = FALSE)
+  all_names <- read_csv("/workdir/tests/data/team_stats_column_names.csv", show_col_types = FALSE)
+  names(team) <- all_names$column_names
+  return(team)
+}

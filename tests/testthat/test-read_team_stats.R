05e2c9288511a8bfc14612b31d22e8456e7c890e
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-24 12:15:19 -0700

    🛑🧪 Skip original name row

--- tests/testthat/test-read_team_stats.R
+++ tests/testthat/test-read_team_stats.R
@@ -5,4 +5,9 @@ describe("read data with the right names", {
   expected <- read_csv("/workdir/tests/data/team_stats_column_names.csv", show_col_types = FALSE)
   obtained <- names(liverpool)
   expect_equal(obtained, expected$column_names)
+  it("skips the name row", {
+    chelsea <- read_team_stats("/workdir/tests/data/Chelsea.csv")
+    expect_equal(chelsea$Date[1], "Chelsea")
+    expect_equal(chelsea$Date[2], "Oppponent")
+  })
 })

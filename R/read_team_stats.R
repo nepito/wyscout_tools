dcb3068d1db8705315684ca7c34667a38a5c759e
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-24 11:53:52 -0700

    ♻️ Use internal data

--- R/read_team_stats.R
+++ R/read_team_stats.R
@@ -1,6 +1,5 @@
 read_team_stats <- function(path) {
   team <- readr::read_csv(path, show_col_types = FALSE)
-  all_names <- readr::read_csv("/workdir/tests/data/team_stats_column_names.csv", show_col_types = FALSE)
   names(team) <- all_names$column_names
   return(team)
 }

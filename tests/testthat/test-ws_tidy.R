f4d506ff7571b53da0281a805701e8a7a74fff06
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-24 17:19:39 -0700

    🛑🧪 Test `obtain_passes_to_final_third_of_two_teams_by_match()`

--- tests/testthat/test-ws_tidy.R
+++ tests/testthat/test-ws_tidy.R
@@ -6,3 +6,16 @@ describe("filter_premier_league", {
     unique()
   expect_equal(obtained, expected)
 })
+
+describe("obtain_passes_to_final_third_of_two_teams_by_match", {
+  raw_data <- tibble(
+    Date = rep(c("a", "b", "c"), 2),
+    Passes_to_final_third_accurate = 1:6
+  )
+  expected <- tibble(
+    Date = c("a", "b", "c"),
+    Passes_to_final_third_accurate = c(4, 6, 9)
+  )
+  obtained <- obtain_passes_to_final_third_of_two_teams_by_match(raw_data)
+  expect_equal(obtained, expected)
+})

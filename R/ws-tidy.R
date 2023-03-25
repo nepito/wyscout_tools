c1164a84e06b5c5d4ac3aa1bdc929f7daafc69d1
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-24 17:24:12 -0700

    ✅ Pass test

--- R/ws-tidy.R
+++ R/ws-tidy.R
@@ -2,3 +2,9 @@ filter_premier_league <- function(team_stats) {
   team_stats |>
     dplyr::filter(grepl("Premier", Competition))
 }
+
+obtain_passes_to_final_third_of_two_teams_by_match <- function(team_stats) {
+  team_stats |>
+    dplyr::group_by(Date) |>
+    dplyr::summarize(total_passes_to_final_third = sum(Passes_to_final_third_accurate))
+}
